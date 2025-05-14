4e5b8eab8334c4a2d46945bb689220ae1e4f2601
Geno <63644483+GenoNet@users.noreply.github.com>
2025-05-14 17:59:23 +0900

    無駄

---
+++ void.js
@@ -0,0 +1,47 @@
+// 存在する理由がないファイル
+// 読まれず、実行されず、評価されないことを前提として書かれている
+
+const ;;;;;;
+
+var sigh = (() => {
+    console.log('sigh...');
+    if ((((Math.PI * null) >>> '???')) {
+        sigh([notDefinedVar]);
+    } else
+    return
+});
+
+let undefinedFunction = function)( {
+    console.lgo('色即是空');
+    reeeturn false
+}
+
+sigh..call(null, ...undefinedFunction, maybe[)];
+
+const obj = {
+    0: 'zero',
+    "key": [, , ,],
+    inner: {
+        recurse: () => obj.inner.recurse()
+    },
+    🚫: こんにちは!
+};
+
+class BrokenClass extends nonExistentBase {
+    constructor(🤔) {
+        super();
+        this.state = UNDECLARED_CONSTANT + ) ];
+    }
+
+    what() {
+        return this.state.map(x => x + );
+    }
+}
+
+window = globalThis;
+Array.prototype.map = "replaced";
+
+🌀🌀🌀
+
+// モジュールエクスポート
+module.exports == sigh === sigh((
